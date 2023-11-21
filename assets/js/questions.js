b847f68c3da4c02adb9de2109c2e44e7950dd0ac
Marchopkins96 <>
2023-11-21 12:06:44 +0000

    Add questions.js file and add questions to it

---
+++ assets/js/questions.js
@@ -0,0 +1,19 @@
+// An array that holds all the questions and possible answers, as well as indicating the correct answer.
+const allQuestions = [
+    {
+        question: 'Who is the current all-time NBA leading points scorer?',
+        answers: ['Michael Jordan', 'LeBron James', 'Kobe Bryant', 'Karl Malone', 1],
+    },
+    {
+        question: 'Which NFL team does quarter-back Patrick Mahones play for?',
+        answers: ['Buffalo Bills', 'Philadelphia Eagles', 'Kansas City Chiefs', 'Miami Dolphins', 2],
+    },
+    {
+        question: 'Which Hockey team has won the most stanley cup trophies?',
+        answers: ['Montreal Canadiens', 'Toronto Maple Leafs', 'New York Rangers', 'Boston Bruins', 0],
+    },
+    {
+        question: 'Who has won the most mens singles tennis titles?',
+        answers: ['Bjorn Borg', 'Pete Sampras', 'Roger Federer', 'Novak Djokovic', 3],
+    },
+]
