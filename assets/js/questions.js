a3ef5a6921fd25544f6ad8a2b6707f49bb83a6bf
Marchopkins96 <>
2023-11-21 14:23:07 +0000

    Add more questions to questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -16,4 +16,68 @@ const allQuestions = [
         question: 'Who has won the most mens singles tennis titles?',
         answers: ['Bjorn Borg', 'Pete Sampras', 'Roger Federer', 'Novak Djokovic', 3],
     },
+    {
+        question: 'Which country won the mens FIFA World Cup in 2018?',
+        answers: ['Germany', 'Brazil', 'France', 'Argentina', 2],
+    },
+    {
+        question: 'Which golfer currently holds the record for most major wins?',
+        answers: ['Jack Nicklaus', 'Tiger Woods', 'Ben Hogan', 'Arnold Palmer', 0],
+    },
+    {
+        question: 'Which football team has won the most Champions Leauge trophies?',
+        answers: ['AC Milan', 'Real Madrid', 'Bayern Munich', 'Manchester United', 1],
+    },
+    {
+        question: 'Who is the only athlete to win olympic gold medals in the 100m & 400m?',
+        answers: ['Carl Lewis', 'Usain Bolt', 'Michael Johnson', 'Yohan Blake', 2],
+    },
+    {
+        question: 'Which nation has won the most rugby union world cups?',
+        answers: ['England', 'New Zealand', 'France', 'South Africa', 3],
+    },
+    {
+        question: 'Which footballer is the current all time Premier Leauge top goal scorer?',
+        answers: ['Wayne Rooney', 'Harry Kane', 'Alan Shearer', 'Sergio Aguero', 2],
+    },
+    {
+        question: 'In darts, what is the highest score of a single dart?',
+        answers: ['40', '60', '100', '80', 1],
+    },
+    {
+        question: 'Who has the current most F1 race wins?',
+        answers: ['Max Verstappen', 'Sebastien Vettel', 'Lewis Hamilton', 'Michael Schumacher', 2],
+    },
+    {
+        question: 'Which boxer did Muhammad Ali fight in "The rumble in the jungle?"',
+        answers: ['George Forman', 'Joe Frazier', 'Sonny Liston', 'Larry Holmes', 0],
+    },
+    {
+        question: 'Which French city is famous for its 24hr motor race?',
+        answers: ['Paris', 'Marseille', 'Lille', 'Le Mans', 3],
+    },
+    {
+        question: 'Jessica  Enniss-Hill competed for Great Britain in which sport?',
+        answers: ['Heptathlon', 'Long Jump', 'Triathlon', 'Pole Vault', 0],
+    },
+    {
+        question: 'Which nation won the 2019 cricket world cup?',
+        answers: ['Australia', 'England', 'India', 'Sri Lanka', 1],
+    },
+    {
+        question: 'The Chicago Cubs & Boston Red Sox play which sport?',
+        answers: ['Baseball', 'Ice Hockey', 'American Football', 'Basketball', 0],
+    },
+    {
+        question: 'Which footballer has scored the most goals in world cup history?',
+        answers: ['Lionel Messi', 'Cristiano Ronaldo', 'Miroslav Klose', 'Diego Maradona', 2],
+    },
+    {
+        question: 'Which female tennis player has won the most grand slam titles?',
+        answers: ['Serena Williams', 'Stefi Graf', 'Billie Jean King', 'Margaret Court', 3],
+    },
+    {
+        question: 'Which nation call their rugby team the Springboks?',
+        answers: ['Australia', 'South Africa', 'Fiji', 'Samoa', 1],
+    }
 ]
