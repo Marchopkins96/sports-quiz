75a0d2fbe842bc0dc5f75dbe742eb32b0be539be
Marchopkins96 <>
2023-11-22 14:19:23 +0000

    Add functions required for end.js

--- assets/js/end.js
+++ assets/js/end.js
@@ -4,3 +4,33 @@ let finalScoreUser = document.getElementById("final-score-user");
 let mostRecentScore = localStorage.getItem("mostRecentScore");
 let userLevel = localStorage.getItem("userLevel");
 let user = window.localStorage.getItem("username");
+
+//personalized message to appear for the user
+finalScoreUser.innerText = `${user} Your final score was:` ;
+finalScore.innerText = `${mostRecentScore} @ ${userLevel} Level` ;
+
+const playagain = document.getElementById("play-again-btn");
+playagain.addEventListener('click', returntostart);
+
+/**
+ * This function allows a user to start the quiz again
+ */
+function returntostart(e) {
+    load(startOver);
+}
+
+/**
+ * This function delays the loading of myURL
+ * URL is loaded with chosen time delay
+ */
+
+function load(myURL) {
+    setTimeout(myURL, 0);
+}
+
+/**
+ * This function loads the last page when the game is over
+ */
+function startOver() {
+    windown.location.assign('index.html');
+}
