014f6b8f21503b60abe24156380303256792b194
Marchopkins96 <>
2023-11-22 14:04:04 +0000

    Add initial code to end.js file

---
+++ assets/js/end.js
@@ -0,0 +1,6 @@
+// Variables stored in local storage are retrieved
+let finalScore = document.getElementById("final-score");
+let finalScoreUser = document.getElementById("final-score-user");
+let mostRecentScore = localStorage.getItem("mostRecentScore");
+let userLevel = localStorage.getItem("userLevel");
+let user = window.localStorage.getItem("username");
